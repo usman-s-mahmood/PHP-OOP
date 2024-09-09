90217a30e794dff5644a2a4c173bbb486ee6ac51
usman.shahid <usmanshahid027@outlook.com>
2024-09-09 15:31:51 -0700

    done with method over riding, starting abstract class

--- 5_method_overriding/index.php
+++ 5_method_overriding/index.php
@@ -1,10 +1,35 @@
 <?php
 
-class A {
+use function PHPSTORM_META\override;
+
+class Base {
     public $name;
-    public function show() {
-        echo "" . "<br>";
+
+    function __construct($name="None") {
+        $this->name = $name;
+    }
+
+    function show() {
+        echo "Name is: {$this->name}\n" ;
+    }
+
+    function calculate($a, $b) {
+        return $a + $b;
     }
 }
+
+class Derived extends Base {
+    function show() {
+        echo "Derived Constructor\n{$this->name}\n";
+    }
+
+    function calculate($a, $b) {
+        return $a * $b;
+    }
+}
+
+$test = new Derived("Ahmad");
+
+$test->show();
 
 ?>
