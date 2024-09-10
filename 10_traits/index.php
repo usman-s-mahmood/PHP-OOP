1e5b3e22596f4caab398f57b6c8c549810ed91c5
usman.shahid <usmanshahid027@outlook.com>
2024-09-09 18:57:51 -0700

    completed working on traits

--- 10_traits/index.php
+++ 10_traits/index.php
@@ -1,6 +1,28 @@
 <?php
 
+# trait contains functions and attributes that can be used in various classes. An object of trait can not be instantiated yet a the methods and attributes defined within the trait can be used in any class
 
+trait Test {
+    public $data = 1;
+    public function greet() {
+        echo "Hello!\n";
+    }
+}
 
+class Base1 {
+    use Test;
+}
+
+class Base2 {
+    use Test;
+}
+
+$obj1 = new Base1();
+$obj1->greet();
+
+$obj2 = new Base2();
+$obj2->greet();
+
+echo $obj1->data . "\n";
 
 ?>
