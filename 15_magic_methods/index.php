ec7bc8745dbe163dedec541fe409435a56cdec19
usman.shahid <usmanshahid027@outlook.com>
2024-09-11 13:27:50 -0700

    completed working on magic methods

--- 15_magic_methods/index.php
+++ 15_magic_methods/index.php
@@ -1,4 +1,23 @@
 <?php
+// an example of magic methods is __construct()
+// all magic methods start with __ like dunder methods in python
+/*
+some important magic methods
+__construct()
+__destruct()
+__get()
+__set()
+__isset()
+__unset()
+__autoload()
+__clone()
+__sleep()
+__wakeup()
+__call()
+__callStatic()
+__toString()
+__invoke()
+*/
 
 
 
