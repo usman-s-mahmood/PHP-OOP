37930944ba1a3c6c1987240326f78aa94426c858
usman.shahid <usmanshahid027@outlook.com>
2024-09-11 13:21:53 -0700

    completed working on method chaining

--- 14_method_chaining/index.php
+++ 14_method_chaining/index.php
@@ -1,4 +1,28 @@
 <?php
 
 
+class abc {
+    public function first() {
+        echo "This is first\n";
+        return $this;
+    }
+
+    public function second() {
+        echo "This is second\n";
+        return $this;
+    }
+
+    public function third() {
+        echo "This is third\n";
+        return $this;
+    }
+}
+
+$t = new abc();
+$t->first();
+$t->second();
+$t->third();
+$t->first()->second()->third();
+
+
 ?>
