b644efd0feb6985b885a07eee4e8fec856d8e04e
usman.shahid <usmanshahid027@outlook.com>
2024-09-11 13:44:40 -0700

    completed working on __destruct magic function

--- 16_destruct/index.php
+++ 16_destruct/index.php
@@ -1,7 +1,26 @@
 <?php
 
 
+class Base {
+    private $conn;
+    function __construct() {
+        $this->conn = mysqli_connect(
+            $hostname="localhost",
+            $username="root",
+            $password="",
+            $database="test",
+            $port=3306
+        );
+        echo "Base class Constructor called!\n";
+    }
+
+    function __destruct() {
+        echo "Base class DESTRUCTOR called!\n";
+        mysqli_close($this->conn);
+    }
+}
 
 
+$obj = new Base();
 
 ?>
