47d8fb90e46e23f67c2d619a78bd39b64bd1c9cf
usman.shahid <usmanshahid027@outlook.com>
2024-09-11 12:52:33 -0700

    completed working on type hintingphp c:\xampp\htdocs\PHP-OOP\12_type_hinting\index.php

--- 12_type_hinting/index.php
+++ 12_type_hinting/index.php
@@ -1,7 +1,52 @@
 <?php
+// specifying the datatype of function arguments
+
+function sum(int $value) {
+    echo $value + 10 . "\n";
+}
+
+function fruits(array $names) {
+    foreach ($names as $name ) 
+        echo $name . "\n";
+}
+
+sum(3);
+$fr = ["apple", "mango", "kiwi", "watermelon"];
+
+fruits($fr);
+
+class Base {
+    public function greet() {
+        echo "Hello! from Base::greet()\n";
+    }
+
+    public function printName($name) {
+        foreach ($name->names() as $n)
+            echo $n . "\n";
+    }
+}
+
+class OtherBase {
+    public function bye() {
+        echo "Good Bye! from OtherBase::bye()\n";
+    }
+
+    public function names() {
+        return ["Afridi", "Baber", "Rizwan"];
+    }
+}
 
 
+function useObject(OtherBase $obj) {
+    $obj->bye();
+}
 
+useObject(new OtherBase());
 
+$obj = new OtherBase();
+
+$useObj = new Base();
+
+$useObj->printName($obj);
 
 ?>
