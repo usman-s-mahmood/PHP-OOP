78a697f4da5afe34f549c74a8ef8d97b96cf4040
usman.shahid <usmanshahid027@outlook.com>
2024-09-14 18:12:14 -0700

    completed working on set method

--- 19_set/index.php
+++ 19_set/index.php
@@ -1,7 +1,31 @@
 <?php
 
 
+class Base {
+    private $name;
 
+    public function __set($property, $value) {
+        if (property_exists($this, $property))  {
+            $this->$property = $value;
+            echo "$property set to $value\n";
+            return ;
+        }
+        echo "This is a private or non existing property!\n";
+    }
 
+    public function display() {
+        echo "{$this->name}\n";
+    }
+}
+
+// similar to get but the point of it's usage is that it does not allow assignment of value to a private or non existing ppt.
+function main() {
+    $obj1 = new Base();
+    $obj1->test = "This is set!";
+    $obj1->name = "Usman Shahid";
+    $obj1->display();
+}
+
+main()
 
 ?>
