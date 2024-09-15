bc0240469a31665452ebbe5aeae6ae144a417f8f
usman.shahid <usmanshahid027@outlook.com>
2024-09-14 18:43:31 -0700

    completed working on callStatic

--- 21_callStatic/index.php
+++ 21_callStatic/index.php
@@ -2,12 +2,24 @@
 
 
 class Base {
-
+    private static function greet() {
+        echo "Hello World!\n";
+    }
+    public static function __callStatic($method, $args) {
+        if (method_exists(__class__, $method)) {
+            call_user_func_array(
+                [__class__, $method],
+                $args
+            );
+            return ;
+        }
+        echo "This is a private level method with static attribution!\n";
+    }
 }
 
 
 function main() {
-    $obj1 = new Base();
+    Base::greet();
 }
 
 main();
