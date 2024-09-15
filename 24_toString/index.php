e8d001fd36789d06e5ee324a29e9b49313ae50d2
usman.shahid <usmanshahid027@outlook.com>
2024-09-15 11:08:19 -0700

    completed working on toString method

--- 24_toString/index.php
+++ 24_toString/index.php
@@ -1,7 +1,19 @@
 <?php
 
+class Base {
+    public $name;
 
+    public function __toString() {
+        return "Object of class: " . get_class($this) . "\n";
+    }
+    
+}
 
+function main() {
+    $obj1 = new Base();
+    echo $obj1 . "\n";
+}
 
+main();
 
 ?>
