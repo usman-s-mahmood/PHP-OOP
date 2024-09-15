c5baca068a88670924681961b5434eec6f8ec307
usman.shahid <usmanshahid027@outlook.com>
2024-09-14 17:43:07 -0700

    completed working on get method

--- 18_get/index.php
+++ 18_get/index.php
@@ -1,7 +1,34 @@
 <?php
 
+// if any non existing or private property or method is accessed then default behaviour is fatal error. __get() helps to give a custom warning 
+class Base {
+    private $ppt1 = 0;
+    private $ppt2 = 0.1;
+    private $arr = ['c'=>"Dennis Ritchie", "python" => "Guido Van Rossum"];
 
+    public function __get($property) {
+        if (array_key_exists($property, $this->arr))
+        {
+            echo $this->data[$property] . "\n";
+            return $this->data[$property];
+        }
+        else
+        {
+            echo "Key not found in the array!\n";
+            return ;
+        }
+        echo "This is private, protected or non existing property: $property\n";
+    }
+}
 
+$obj1 = new Base();
+echo $obj1->python;
+print_r($obj1->test); // used for printing array or associative array
 
-
+$test_arr = [
+    "name" => "Usman",
+    "Age" => 21,
+    "is human" => true
+];
+print_r($test_arr);
 ?>
