464a2d99623dcac17856e6532dc4f041b860f78c
usman.shahid <usmanshahid027@outlook.com>
2024-09-17 16:43:22 -0700

    completed initial read operation for MySQLi OOP

--- 32_MySQLi_OOP/index.php
+++ 32_MySQLi_OOP/index.php
@@ -2,7 +2,27 @@
 
 
 function main() {
-    ;
+    $conn = new mysqli(
+        $host="localhost",
+        $username="root",
+        $password="",
+        $database="bankschema",
+        $port=3306
+    );
+
+    if ($conn->connect_error)
+        die('Connection Failed!' . "$conn->connect_error" . "\n");
+    
+    $sql = "select * from bank;";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0)
+        while ($row = $result->fetch_assoc())
+            echo "ID: {$row["bank_id"]} - Name: {$row["bank_name"]}\n";
+    else
+        echo "No result found!\n";
+
+    $conn->close();
 }
 
 main();
