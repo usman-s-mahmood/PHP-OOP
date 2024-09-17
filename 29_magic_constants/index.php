851d76c152225ea1a85941a8754cea065169c284
usman.shahid <usmanshahid027@outlook.com>
2024-09-16 19:41:14 -0700

    completed working on magic constants

--- 29_magic_constants/index.php
+++ 29_magic_constants/index.php
@@ -1,10 +1,35 @@
 <?php
 
+namespace std;
+
+trait test {
+    public function testFun() {
+        echo __TRAIT__ . "\n";
+    }
+}
+
+class Base {
+    use test;
+
+    public $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function display() {
+        echo "$this->name\n";
+        echo __CLASS__ . "\t". __method__ . "\t" . __NAMESPACE__ . "\t". $this->testFun() ."\n";
+    }
+}
 
 function main() {
     echo "Line Number: " . __LINE__ . "\n";
     echo "Directory: " . __DIR__ . "\n";
-    // echo "Directory: " . __DIR__ . "\n";
+    echo "Absolute File Path: " . __FILE__ . "\n";
+    echo "Function Name: " . __Function__ . "\n";
+    $obj1 = new Base("Visual Studio Code");
+    $obj1->display();
 }
 
 main();
