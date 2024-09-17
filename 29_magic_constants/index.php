7b0654dbc58bcda0fe233fb885032b02f3160b52
usman.shahid <usmanshahid027@outlook.com>
2024-09-16 19:21:56 -0700

    started working on magic constants

---
+++ 29_magic_constants/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+function main() {
+    echo "Line Number: " . __LINE__ . "\n";
+    echo "Directory: " . __DIR__ . "\n";
+    // echo "Directory: " . __DIR__ . "\n";
+}
+
+main();
+
+
+?>
