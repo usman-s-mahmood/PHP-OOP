16cb72bd18e2d636d89735a7053c0ee5e87bba7b
usman.shahid <usmanshahid027@outlook.com>
2024-09-16 17:39:06 -0700

    completed working on sleep method

--- 25_sleep/index.php
+++ 25_sleep/index.php
@@ -1,12 +1,36 @@
 <?php
 
 class Base {
+    public $name;
+    private $salary;
+    private $id_card;
+    public function __construct($name, $salary, $id_card) {
+        $this->name = $name;
+        $this->salary = $salary;
+        $this->id_card = $id_card;
+    }
+
+    public function __sleep() {
+        return array(
+            'salary',
+            'id_card'
+        );
+    }
 
 }
 
 
 function main() {
-    ;
+    ; // when an object of a class is to be stored in the database, file or session then it is first serialized but in case of OOP the object would be serialized(converted to array) with all of it's properties. To avoid this behaviour, sleep method is used for serializing the necessary attributes of the class
+
+    $obj = new Base(
+        $name="Usman Shahid",
+        $salary=70000,
+        $id_card='1837419hpqufue'
+    );
+
+    $sl = serialize($obj);
+    print($sl);
 }
 
 main();
