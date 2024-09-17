9fcb5d326822c61dc5524eb5823e13ea754d6c06
usman.shahid <usmanshahid027@outlook.com>
2024-09-17 16:53:20 -0700

    added template for database.php

---
+++ 33_CRUD_class/database.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class Database {
+    public function __construct() {
+
+    }
+
+    public function insert() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+
+    public function select() {
+
+    }
+
+    public function __destruct() {
+        
+    }
+}
+
+
+
+?>
