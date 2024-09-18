a9ab182c22506cb04770b1531e62aa89178500b1
usman.shahid <usmanshahid027@outlook.com>
2024-09-17 19:55:39 -0700

    completed insert function for CRUD project

--- 33_CRUD_class/database.php
+++ 33_CRUD_class/database.php
@@ -34,8 +34,34 @@ class Database {
 
     }
 
-    public function insert() {
-
+    public function insert($table, $params=array()) {
+        if ($this->tableExists($table)) {
+            $table_columns = implode(
+                ',', 
+                array_keys($params)
+            );
+            $table_value = implode(
+                "','", 
+                $params
+            );
+            $sql = "INSERT INTO $table ($table_columns) values ('$table_value');";
+            if ($this->mysqli->query($sql)) {
+                array_push(
+                    $this->result,
+                    $this->mysqli->insert_id
+                );
+                return true;
+            }
+            else {
+                array_push(
+                    $this->result,
+                    $this->mysqli->error
+                );
+                return false;
+            }
+        }
+        else 
+            return false;
     }
 
     public function update() {
@@ -48,6 +74,30 @@ class Database {
 
     public function select() {
 
+    }
+
+    private function tableExists($table) {
+        $sql = "SHOW TABLES FROM $this->database LIKE '$table'";
+        $tableInDb = $this->mysqli->query($sql);
+        if ($tableInDb)
+        {
+            if ($tableInDb->num_rows == 1)
+                return true;
+            else {
+                array_push(
+                    $this->result,
+                    $table . " Does Not Exists!"
+                );
+                return false;
+            }
+        }
+
+    }
+
+    public function getResult() {
+        $val = $this->result;
+        $this->result = array();
+        return $val;
     }
 
     public function __destruct() {
--- 33_CRUD_class/index.php
+++ 33_CRUD_class/index.php
@@ -10,6 +10,28 @@ function main() {
     else 
         echo "Failed Connection!\n";
 
+    $passwd_hash = password_hash(
+        'MyPassword10840528',
+        PASSWORD_DEFAULT
+    );
+    // echo $passwd_hash . "\n";
+    // echo "Current Date time: " . implode(', ', getdate()) . "\n";
+    // $date = getdate();
+    // echo getdate()["mday"] . "-" . $date["mon"] . "-" . $date["year"] . "\n";
+    $date = getdate()["year"] . "-" . getdate()["mon"] . "-" . getdate()["mday"];
+    $obj->insert(
+        'users',
+        [
+            'username' => 'anotherone_php_user',
+            'email' => 'test3@mail.com',
+            'password' => password_hash(
+                $password='somepassword123',
+                $algo=PASSWORD_DEFAULT
+            ),
+            'created_at' => $date
+        ]
+    );
+    print_r($obj->getResult());
 }
 
 main();
